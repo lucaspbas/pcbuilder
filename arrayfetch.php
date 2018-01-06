37f27784bfabe66c6824b3004f158b59bfbc37a4
Lucas Bastos <lucasbastos811@gmail.com>
2018-01-05 22:14:57 -0500

    Update arrayfetch.php

--- arrayfetch.php
+++ arrayfetch.php
@@ -2,10 +2,10 @@
 $arraytype = array("0" => "amdcpu","1" => "intelcpu","2" => "amdgpu","3" => "nvidiagpu","4" => "psu","5" => "ram","6" => "storage","7" => "motherboard");
 function arrayFetch($arraytype, $arraycategories) {
 	$connection = mysql_connect('####', '#####', '####');
-	mysql_select_db('lucasbastos');
+	mysql_select_db('#####');
 	for($i = 0, $size = count($arraytype); $i < $size; $i++) {
 		$type = (string) $arraytype[$i];
-    	$query = "SELECT * FROM pcbuilderarray WHERE type = '$type' ORDER BY arrayID"; 
+    	$query = "SELECT * FROM #### WHERE type = '$type' ORDER BY arrayID"; 
 		$result = mysql_query($query);
 		$array[$type] = array();
 		while($row = mysql_fetch_assoc($result)){
